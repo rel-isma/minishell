a13120f388befa5b58f628ccf7545f5752052c4f
Youssri El Bassri <yoel-bas@e3r10p12.1337.ma>
2023-08-20 22:49:00 +0100

    exit

--- src/exec/ft_exit.c
+++ src/exec/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: yoel-bas <yoel-bas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 10:13:45 by yoel-bas          #+#    #+#             */
-/*   Updated: 2023/08/20 22:07:43 by yoel-bas         ###   ########.fr       */
+/*   Updated: 2023/08/20 22:42:21 by yoel-bas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ int	syntax_exit(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if(!ft_atoi(str))
-			return(1);
-		if ((str[0] != '+' && str[0] != '-') && !ft_isdigit(str[i]))
+		if (((str[0] != '+' && str[0] != '-') && !ft_isdigit(str[i])) || (str[1] == '+' || str[1] == '-'))
 			return (1);
 		i++;
 	}
@@ -82,19 +80,16 @@ int	ft_exit_builtin(t_list *tmp)
 	if (((t_cmd *)(tmp->content))->argms[1])
 	{
 		if (failed_exit(tmp))
-			exit(1);
-		status = ft_atoi(((t_cmd *)(tmp->content))->argms[1]);
-		if (status > 255)
 		{
-			status %= 256;
-			g_minishell.exit_code = status;
+			g_minishell.exit_code = 1;
 		}
-		else if (status < 0)
-			g_minishell.exit_code = 156;
-		else
-			g_minishell.exit_code = status;
-		printf("exit\n");
+		else 
+		{
+		status = ft_atoi(((t_cmd *)(tmp->content))->argms[1]);
+		g_minishell.exit_code = status;
+		printf("exit \n");
 		exit(g_minishell.exit_code);
+		}
 	}
 	else
 		printf("exit\n");
